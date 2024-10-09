2919cd66deea863d4131ef80ebaab7670c82eab6
Reed Salus <reedsalus@gmail.com>
2024-10-09 14:57:19 -0700

    remove annotations and simplify context

--- StarterMicroservice/StarterMicroservice.API/MyUser.cs
+++ StarterMicroservice/StarterMicroservice.API/MyUser.cs
@@ -1,25 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace StarterMicroservice.API
+﻿namespace StarterMicroservice.API
 {
-    [Index(nameof(UserName), IsUnique = true)]
     public class MyUser
     {
-        [Key]
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
-
-        [Required]
-        [StringLength(128)]
-        public required string UserName { get; set; }
-
-        [StringLength(128)]
-        public string? Name { get; set; }
-
-        [StringLength(255)]
-        public string? Email { get; set; }
     }
 }
